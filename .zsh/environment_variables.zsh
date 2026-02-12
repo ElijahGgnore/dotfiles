770c0fb2982919524d035f1cc0a31f1ea0a73a0e
ElijahGgnore <elijahg.gnore@gmail.com>
2026-02-12 21:19:33 +1000

    fix(zsh): fix nvim check

--- .zsh/environment_variables.zsh
+++ .zsh/environment_variables.zsh
@@ -4,4 +4,8 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000 # N of lines to read at the start of the session
 SAVEHIST=10000 # N of lines to save at the end of the session
 
-[ -x nvim ] && export MANPAGER='nvim +Man!' # If nvim is available, use it as a man pager
+if command -v nvim >/dev/null 2>&1 # If nvim is available, use it as a man pager and default editor
+then
+        export MANPAGER='nvim +Man!'
+        export EDITOR='nvim'
+fi
