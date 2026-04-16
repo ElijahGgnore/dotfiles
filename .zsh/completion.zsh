f8d2145e06bf5240772f6424a081948104b8d78e
ElijahGgnore <elijahg.gnore@gmail.com>
2026-04-16 23:19:18 +1000

    feat(zsh): make completion case-insensitive

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,6 +1,10 @@
 autoload -Uz compinit
 compinit
 zstyle ':completion:*' menu select
+
+# case-insensitive completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+
 zstyle ':completion::complete:*' gain-privileges 1
 
 # Set up fzf key bindings and fuzzy completion
