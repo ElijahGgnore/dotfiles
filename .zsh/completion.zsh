b2cbc201d0f066d51b93ec0568522c62ecaa9dbd
ElijahGgnore <elijahg.gnore@gmail.com>
2026-06-14 18:39:47 +1000

    docs(zsh): add fzf binding hints

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -8,4 +8,7 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion::complete:*' gain-privileges 1
 
 # Set up fzf key bindings and fuzzy completion
+# Ctrl+t list files+folders in current director
+# Ctrl+r search history of shell commands
+# Alt+c fuzzy change directory
 source <(fzf --zsh)
